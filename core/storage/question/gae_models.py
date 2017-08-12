9273e0a6e2d40f8db45fdb1d8ee3832bd5d88605
Manoj Mohan <m.manoj@linuxmail.org>
2017-08-12 22:49:18 +0530

    created QuestionModel inside storage and added basic stubs

---
+++ core/storage/question/gae_models.py
@@ -0,0 +1,12 @@
+import core.storage.base_model.gae_models as base_models
+from google.appengine.ext import ndb
+from constants import constants
+
+
+class QuestionModel(base_models.VersionedModel):
+    question_dict = ndb.JsonProperty(default={}, index=False)
+    question_title = ndb.StringProperty(indexed=True)
+    question_schema_version = ndb.IntegerProperty(index=False)
+    collection_id = ndb.IntegerProperty(indexed=True)
+    language_code = ndb.StringProperty(default=constants.DEFAULT_LANGUAGE_CODE,
+                                       indexed=True)
