cdc07da3930fc2bc542518f2840ec5f7399cb827
Manoj Mohan <m.manoj@linuxmail.org>
2017-08-15 19:43:29 +0530

    fixed minor linting issues

--- core/storage/question/gae_models.py
+++ core/storage/question/gae_models.py
@@ -4,9 +4,9 @@ from constants import constants
 
 
 class QuestionModel(base_models.VersionedModel):
-    question_dict = ndb.JsonProperty(default={}, index=False)
+    question_dict = ndb.JsonProperty(default={}, indexed=False)
     question_title = ndb.StringProperty(indexed=True)
-    question_schema_version = ndb.IntegerProperty(index=False)
+    question_schema_version = ndb.IntegerProperty(indexed=False)
     collection_id = ndb.IntegerProperty(indexed=True)
     language_code = ndb.StringProperty(default=constants.DEFAULT_LANGUAGE_CODE,
                                        indexed=True)
