0044200fc68253f791a00dde602c5d24932622d1
Manoj Mohan <m.manoj@linuxmail.org>
2017-08-20 23:10:21 +0530

    added stubs _create_question and improved save_new_question

--- core/domain/question_services.py
+++ core/domain/question_services.py
@@ -1,8 +1,15 @@
 from core.domain import question_domain
 
 
-def save_new_question():
+def _create_question(commiter_id, question, commit_message, commit_cmds):
     pass
+
+
+def save_new_question(commiter_id, question):
+    """ Saves a new question. """
+   commit_message = (
+   'New question created with title \'%s\'.' % question.question_title)
+   _create_question(committer_id, collection, commit_message, [{}])
 
 
 def delete_question():
