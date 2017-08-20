ff0bee0a93821ac7afa7a196f2bd717f5f2bad11
Manoj Mohan <m.manoj@linuxmail.org>
2017-08-20 23:14:09 +0530

    linting issues fix

--- core/domain/question_services.py
+++ core/domain/question_services.py
@@ -1,15 +1,14 @@
-from core.domain import question_domain
 
 
-def _create_question(commiter_id, question, commit_message, commit_cmds):
+def _create_question(committer_id, question, commit_message, commit_cmds):
     pass
 
 
-def save_new_question(commiter_id, question):
+def save_new_question(committer_id, question):
     """ Saves a new question. """
-   commit_message = (
-   'New question created with title \'%s\'.' % question.question_title)
-   _create_question(committer_id, collection, commit_message, [{}])
+    commit_message = (
+    'New question created with title \'%s\'.' % question.question_title)
+    _create_question(committer_id, question, commit_message, [{}])
 
 
 def delete_question():
